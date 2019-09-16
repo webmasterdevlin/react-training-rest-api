da07b6b719c59d1e3e96e9f6f36589bf47996960
Devlin Duldulao <devlin.duldulao@inmeta.no>
2019-09-16 23:29:54 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [members, setMembers] = useState([]);
+  const [member, setMember] = useState({});
+
+  useEffect( () => {
+      console.log("USE_EFFECT!");
+      loadMembers();
+  }, []);
+
+const loadMembers = async () => {
+  const { data } =  await axios.get("http://localhost:5000/members")
+  console.log( "RESPONSE: ", data);
+  setMembers(data);
+}
+
+const deleteMember = async (id) => {
+  const previousMembers = [...members];
+   const newMembers = members.filter(m => m.id !== id);
+    setMembers(newMembers);
+ try {
+    await axios.delete(`http://localhost:5000/members/${id}`);
+ } catch (e) {
+   setMembers(previousMembers);
+   alert(e.message);
+ }
+}
+
+const handleNameOnChange = ({currentTarget}) => {
+  setMember({...member, name: currentTarget.value});
+}
+const  handleAgeOnChange = ({currentTarget}) => {
+  setMember({...member, age: currentTarget.value});
+}
+const handleAddMember = async () => {
+  try {
+      const {data : createdMember} = await axios.post("http://localhost:5000/members", member);
+      const newMemberCollection =  [...members, createdMember]
+      setMembers(newMemberCollection);
+  } catch (e) {
+        alert(e.message)
+  }
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React Demo</h2>
+      <input type="text" onChange={handleNameOnChange} />
+      <input type="text" onChange={handleAgeOnChange} />
+      <button onClick={handleAddMember}>Add Member</button>
+      <h3>
+        <ul>
+          {members.map(m => 
+          <li key={m.id}>name: {m.name}, age: {m.age}
+            <button onClick={() => deleteMember(m.id)} >Delete</button>
+          </li>
+          )}
+        </ul>
+      </h3>
     </div>
   );
 }
